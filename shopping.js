1eb0d128bdcb49bdad6ef519ca8250798cc44f35
jaiswalsachin4708 <sachinjaiswal4708@gmail.com>
2022-10-24 08:52:48 +0530

    Ecommerce shopping website

---
+++ shopping.js
@@ -0,0 +1,35 @@
+
+// for menu bar
+const bar=document.getElementById('bar');
+const close=document.getElementById('close');
+const nav=document.getElementById('navbar');
+
+if(bar){
+    bar.addEventListener('click', () =>{
+        nav.classList.add('active');
+    })
+}
+
+if(close){
+    close.addEventListener('click', () =>{
+        nav.classList.remove('active');
+    })
+}
+
+
+// for changing the images
+var Maimg = document.getElementById("Mainimg");
+        var smallimg = document.getElementsByClassName("smalimg");
+
+        smallimg[0].onclick = function () {
+            mainimg.src = smallimg[0].src;
+        }
+        smallimg[1].onclick = function () {
+            mainimg.src = smallimg[1].src;
+        }
+        smallimg[2].onclick = function () {
+            mainimg.src = smallimg[2].src;
+        }
+        smallimg[3].onclick = function () {
+            mainimg.src = smallimg[3].src;
+        }
